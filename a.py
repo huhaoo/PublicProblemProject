e76d17d064335311783cfad16efca62e34196ec4
huhaoo <qq826538400@gmail.com>
2021-01-27 16:44:36 +0800

    first commit

--- a.py
+++ a.py
@@ -1,12 +1,18 @@
 import requests
+import time
 
-def AtcoderGet(name,_id):
-    url = f'https://atcoder.jp/contests/'+_id+'/submissions?f.Task=&f.LanguageName=&f.Status=&f.User='+name
+def run(name,_id):
+    #print(name,_id)
+    url = f'https://atcoder.jp/contests/'+_id+'/submissions?f.Task=&f.LanguageName=&f.Status=AC&f.User='+name
     #print(url)
-    r = requests.get(url, timeout = 10000)
+    r = requests.get(url, timeout = 5)
     #print(r)
     r.encoding = 'utf-8'
     file=open("a.html", mode='w')
     file.write(r.text)
-for i in range(1,10):
-    AtcoderGet("pengzhike","agc00"+str(i))
+    time.sleep(10)
+user_list=["pengzhike","skydogli","ljfcnyali"]
+id_list=["agc001","agc002"]
+for i in user_list:
+    for j in id_list:
+       run(i,j) 
