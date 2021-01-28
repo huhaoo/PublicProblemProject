f3fe0e99cdc67d72443c72eb3c18b8066dee8c85
ljfcnyali <ljfcnyali@gmail.com>
2021-01-28 15:18:34 +0800

    彻底完成Atcoder查表代码

--- a.py
+++ a.py
@@ -2,11 +2,15 @@ import requests
 import time
 import datetime
 import smtplib
+import re
 from email.mime.text import MIMEText
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
 
 user_list=["pengzhike","skydogli","ljfcnyali","khronos"]
-#_time = str(datetime.date.today())
-_time = "2021-01-27"
+_time = time.localtime(time.time())
+#  _time = [2020,12,22]
 
 ################################   Send mail part   ######################################
 smtp = smtplib.SMTP() 
@@ -85,14 +89,26 @@ mail(allinfo)
 smtp.quit()
 """
 
-def Run(s):
-    pass
+def Run(s, name):
+    lstpos = 0
+    while True :
+        pos = s.find("AC", lstpos)
+        if pos == -1:
+            return
+        lstpos = pos + 1
+        t = s[:pos]
+        y = t.rfind("epoch_second")
+        x = time.gmtime((int)(re.search("\d+", s[y:]).group()) + 28800)
+        if x[0] == _time[0] and x[1] == _time[1] and x[2] == _time[2] :
+            print(time.strftime("%Y-%m-%d %H:%M:%S", x), end=' ')
+            print(name+" Accepted "+re.search("[a-z]+[0-9]+_[a-z]?", s[s.find("problem_id", y):]).group())
+            
 
 def Get(name):
     url = f'https://kenkoooo.com/atcoder/atcoder-api/results?user='+name
     r = requests.get(url, timeout = 1000)
     r.encoding = 'utf-8'
-    Run(r.text)
+    Run(r.text, name)
     time.sleep(1)
 
 for i in user_list:
