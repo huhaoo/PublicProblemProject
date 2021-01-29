734dacf6a7a78f404e61634ed456d7e095d0537d
ljfcnyali <ljfcnyali@gmail.com>
2021-01-29 14:46:14 +0800

    huhaoo AC Submissions

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import a
 from mail import mail
 
-user_list=["pengzhike","skydogli","ljfcnyali","khronos","CraZYali"]
+user_list=["pengzhike","skydogli","ljfcnyali","khronos","CraZYali","huhaoo"]
 
 s = ""
 for i in user_list:
+    print(i)
     s = s + a.Get(i)
 
 mail(s)
