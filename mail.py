25efe6d3103de8ad379a099e79ae21e6df28ef7d
ljfcnyali <ljfcnyali@gmail.com>
2021-03-12 11:31:10 +0800

    Fix a lot of bugs & Create new bugs

--- mail.py
+++ mail.py
@@ -5,21 +5,23 @@ def mail(s):
     if len(s)<2:
         return
     smtp = smtplib.SMTP() 
-    smtp.connect('smtp.163.com')
     sender=""
     password=""
+    add=""
     try:
         f=open("password","r")
     except:
-        print("Please input your mail's address & password.")
+        print("Please input your mail's address & password & sender address.(e.g. mail.163.com => smtp.163.com)")
         sender=input()
         password=input()
+        add=input()
         F=open("password","w")
         print(sender,file=F)
         print(password,file=F)
     else:
         sender,password=f.read().split()
     try:
+        smtp.connect(add)
         smtp.login(sender,password)
     except:
         print(sender,'/',password)
